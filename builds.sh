5d2dd1d404d77c2c62a22c86801a399aab5692a7
evandromartines <evandromartines@gmail.com>
2024-09-23 01:08:37 -0300

    organize scripts and source, add redhat vm, adoptium

--- builds.sh
+++ builds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #RED HAT VM
-#docker build --no-cache -t springboot-imperative-redhat:jvm21 -f dockers/RedHat-VM/Dockerfile.springboot.imperative .
-#docker build --no-cache -t springboot-reactive-redhat:jvm21 -f dockers/RedHat-VM/Dockerfile.springboot.reactive .
+docker build --no-cache -t springboot-imperative-redhat:jvm21 -f dockers/RedHat-VM/Dockerfile.springboot.imperative .
+docker build --no-cache -t springboot-reactive-redhat:jvm21 -f dockers/RedHat-VM/Dockerfile.springboot.reactive .
 
 #ADOPTIUM ECLIPSE VM
 docker build --no-cache -t springboot-imperative-adoptium:jvm21 -f dockers/Adoptium-VM/Dockerfile.springboot.imperative .
